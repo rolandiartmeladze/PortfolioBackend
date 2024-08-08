124f4d4c4920ff8e51ea7345c45c5422d85295da
rolandiartmeladze <rartmeladze@gmail.com>
2024-08-08 15:21:59 +0400

    Add startup script and Vercel configuration

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
-#!/bin/bash
+# Requirements-ის ინსტალაცია
 pip install -r requirements.txt
-python3 app.py 
+
+# Django პროექტის გაშვება
+python3 manage.py migrate
+python3 manage.py runserver 0.0.0.0:8000
 
 chmod +x start.sh
