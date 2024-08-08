6c7ad1f90dcf6a4703d5ee40a6a03d126b704de9
rolandiartmeladze <rartmeladze@gmail.com>
2024-08-08 15:24:02 +0400

    V

--- PortfolioBackend/settings.py
+++ PortfolioBackend/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = 'django-insecure-+j+idkxaz!765zswytzilv^1@ru%e_#rx#cej4(m@zwsqtcx%r
 DEBUG = True
 
 # ALLOWED_HOSTS = ['*.vercel.app']
+ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['portfolio-backend-151yx5eav-roland-artmeladzes-projects.vercel.app', 'localhost']
 
 
--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 # Requirements-ის ინსტალაცია
 pip install -r requirements.txt
 
-# Django პროექტის გაშვება
+# მონაცემთა ბაზის მიგრაცია
 python3 manage.py migrate
-python3 manage.py runserver 0.0.0.0:8000
 
-chmod +x start.sh
+# Django სერვერის გაშვება
+python3 manage.py runserver 0.0.0.0:8000
